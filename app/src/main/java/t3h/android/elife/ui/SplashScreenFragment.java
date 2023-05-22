737f3c26edccbb91ebeef5347cf38d01ba41ffbd
dkifashion <dkiwomanfashion@gmail.com>
2023-05-22 09:47:42 +0700

    init project again and build splash screen

---
+++ app/src/main/java/t3h/android/elife/ui/SplashScreenFragment.java
@@ -0,0 +1,39 @@
+package t3h.android.elife.ui;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import t3h.android.elife.R;
+import t3h.android.elife.databinding.FragmentSplashScreenBinding;
+
+public class SplashScreenFragment extends Fragment {
+    private FragmentSplashScreenBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        requireActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        binding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_splash_screen, container, false);
+        View view = binding.getRoot();
+
+        Animation topAnim = AnimationUtils.loadAnimation(requireActivity(), R.anim.top_animation);
+        Animation bottomAnim = AnimationUtils.loadAnimation(requireActivity(), R.anim.bottom_animation);
+        binding.logo.setAnimation(topAnim);
+        binding.appName.setAnimation(bottomAnim);
+        binding.slogan.setAnimation(bottomAnim);
+        binding.startBtn.setAnimation(bottomAnim);
+
+        return view;
+    }
+}
