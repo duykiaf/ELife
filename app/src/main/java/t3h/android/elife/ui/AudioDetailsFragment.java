4d52355a17b6c85e4a3937e85dcc30a0eda53ecc
dkifashion <dkiwomanfashion@gmail.com>
2023-06-12 22:57:11 +0700

    build audio details ui using viewpager2

--- app/src/main/java/t3h/android/elife/ui/AudioDetailsFragment.java
+++ app/src/main/java/t3h/android/elife/ui/AudioDetailsFragment.java
@@ -2,65 +2,72 @@ package t3h.android.elife.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import t3h.android.elife.R;
+import t3h.android.elife.adapters.FragmentAdapter;
+import t3h.android.elife.databinding.FragmentAudioDetailsBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AudioDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AudioDetailsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AudioDetailsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AudioDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AudioDetailsFragment newInstance(String param1, String param2) {
-        AudioDetailsFragment fragment = new AudioDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentAudioDetailsBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio_details, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_audio_details, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new LyricsFragment());
+        fragmentList.add(new TranslationsFragment());
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(requireActivity(), fragmentList);
+        binding.pager.setAdapter(fragmentAdapter);
+
+        String[] tabLayoutNames = {"Lyrics", "Translations"};
+        new TabLayoutMediator(binding.tabLayout, binding.pager,
+                (tab, position) -> tab.setText(tabLayoutNames[position])
+        ).attach();
+
+//        binding.pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+//            @Override
+//            public void onPageSelected(int position) {
+//                super.onPageSelected(position);
+//                switch (position) {
+//                    case 0:
+//                        break;
+//                    case 1:
+//                        break;
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
