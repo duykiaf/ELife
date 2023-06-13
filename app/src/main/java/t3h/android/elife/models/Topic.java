c46a13191788904b5a2fb30c80c1a6760a2f66ec
dkifashion <dkiwomanfashion@gmail.com>
2023-06-13 21:52:54 +0700

    define Topic object

--- app/src/main/java/t3h/android/elife/models/Topic.java
+++ app/src/main/java/t3h/android/elife/models/Topic.java
@@ -1,2 +1,68 @@
-package t3h.android.elife.models;public class Topic {
+package t3h.android.elife.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Topic implements Serializable {
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    @SerializedName("category_id")
+    @Expose
+    private int categoryId;
+
+    @SerializedName("status")
+    @Expose
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
