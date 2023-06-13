32b56ade67a425475e0e257afa73f6a10759c16c
dkifashion <dkiwomanfashion@gmail.com>
2023-06-13 21:52:37 +0700

    define Audio object

--- app/src/main/java/t3h/android/elife/models/Audio.java
+++ app/src/main/java/t3h/android/elife/models/Audio.java
@@ -1,2 +1,92 @@
-package t3h.android.elife.models;public class Audio {
+package t3h.android.elife.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Audio implements Serializable {
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("audio_file")
+    @Expose
+    private String audioFile;
+
+    @SerializedName("file_name")
+    @Expose
+    private String fileName;
+
+    @SerializedName("lyrics")
+    @Expose
+    private String lyrics;
+
+    @SerializedName("topic_id")
+    @Expose
+    private int topicId;
+
+    @SerializedName("status")
+    @Expose
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAudioFile() {
+        return audioFile;
+    }
+
+    public void setAudioFile(String audioFile) {
+        this.audioFile = audioFile;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public int getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(int topicId) {
+        this.topicId = topicId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
