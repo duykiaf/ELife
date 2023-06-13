f6b09834c85efbdc71614c26f88f5047912c0435
dkifashion <dkiwomanfashion@gmail.com>
2023-06-13 21:58:24 +0700

    get active topics list

--- app/src/main/java/t3h/android/elife/repositories/MainRepository.java
+++ app/src/main/java/t3h/android/elife/repositories/MainRepository.java
@@ -1,2 +1,40 @@
-package t3h.android.elife.repositories;public class MainRepository {
+package t3h.android.elife.repositories;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import t3h.android.elife.api.ApiProvider;
+import t3h.android.elife.api.MainApi;
+import t3h.android.elife.models.Topic;
+
+public class MainRepository {
+    private MainApi mainApi;
+
+    public MainRepository() {
+        mainApi = ApiProvider.getMainApi();
+    }
+
+    public LiveData<List<Topic>> getActiveTopicsList() {
+        MutableLiveData<List<Topic>> liveData = new MutableLiveData<>(new ArrayList<>());
+        mainApi.getActiveTopicsList().enqueue(new Callback<List<Topic>>() {
+            @Override
+            public void onResponse(Call<List<Topic>> call, Response<List<Topic>> response) {
+                liveData.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Topic>> call, Throwable t) {
+                Log.e("ERROR getActiveTopicsList", t.getMessage());
+            }
+        });
+        return liveData;
+    }
 }
