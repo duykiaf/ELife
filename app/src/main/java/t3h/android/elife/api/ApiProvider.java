fa43a88e785dd6a3ccdd7f004e5a8485e5009225
dkifashion <dkiwomanfashion@gmail.com>
2023-06-13 21:54:12 +0700

    config retrofit

--- app/src/main/java/t3h/android/elife/api/ApiProvider.java
+++ app/src/main/java/t3h/android/elife/api/ApiProvider.java
@@ -1,2 +1,30 @@
-package t3h.android.elife.api;public class ApiProvider {
+package t3h.android.elife.api;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiProvider {
+    private static final String BASE_URL = "http://192.168.1.7:3000";
+    private static MainApi mainApi;
+    private static Retrofit retrofit;
+
+    public static MainApi getMainApi() {
+        if (mainApi == null) {
+            mainApi = getRetrofit().create(MainApi.class);
+        }
+        return mainApi;
+    }
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+            retrofit = new Retrofit.Builder()
+                    .client(okHttpClient)
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
