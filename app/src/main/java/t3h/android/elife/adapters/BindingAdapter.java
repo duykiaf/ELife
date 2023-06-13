cc5fc9b5bbd43cd2ece987e82c8d3ad4e2c870a9
dkifashion <dkiwomanfashion@gmail.com>
2023-06-13 22:02:27 +0700

    custom binding adapter to display image by image url

--- app/src/main/java/t3h/android/elife/adapters/BindingAdapter.java
+++ app/src/main/java/t3h/android/elife/adapters/BindingAdapter.java
@@ -1,2 +1,19 @@
-package t3h.android.elife.adapters;public class BindingAdapter {
+package t3h.android.elife.adapters;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import t3h.android.elife.R;
+
+public class BindingAdapter {
+    @androidx.databinding.BindingAdapter(value = {"url"})
+    public static void bindImageView(ImageView view, String url) {
+        Glide.with(view)
+                .load(url)
+                .centerCrop()
+                .placeholder(R.drawable.ic_image)
+                .error(R.drawable.ic_broken_image)
+                .into(view);
+    }
 }
