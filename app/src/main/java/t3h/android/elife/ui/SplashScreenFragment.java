17f02a63148a64fb52f3fafefa5465728461eba4
dkifashion <dkiwomanfashion@gmail.com>
2023-06-13 21:47:10 +0700

    add onViewCreated method and handle start btn click

--- app/src/main/java/t3h/android/elife/ui/SplashScreenFragment.java
+++ app/src/main/java/t3h/android/elife/ui/SplashScreenFragment.java
@@ -8,8 +8,12 @@ import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import t3h.android.elife.R;
 import t3h.android.elife.databinding.FragmentSplashScreenBinding;
@@ -24,21 +28,27 @@ public class SplashScreenFragment extends Fragment {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding =
                 DataBindingUtil.inflate(inflater, R.layout.fragment_splash_screen, container, false);
-        View view = binding.getRoot();
+        return binding.getRoot();
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         Animation topAnim = AnimationUtils.loadAnimation(requireActivity(), R.anim.top_animation);
         Animation bottomAnim = AnimationUtils.loadAnimation(requireActivity(), R.anim.bottom_animation);
         binding.logo.setAnimation(topAnim);
         binding.appName.setAnimation(bottomAnim);
         binding.slogan.setAnimation(bottomAnim);
         binding.startBtn.setAnimation(bottomAnim);
-
-        return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
+        binding.startBtn.setOnClickListener(v ->
+                navController.navigate(R.id.action_splashScreenFragment_to_topicsListFragment)
+        );
     }
 
     @Override
