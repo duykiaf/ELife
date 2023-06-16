3b1055e39678424886497b49dacfa28097e5437d
dkifashion <dkiwomanfashion@gmail.com>
2023-06-16 21:41:00 +0700

    get active audio list by topic id

--- app/src/main/java/t3h/android/elife/api/MainApi.java
+++ app/src/main/java/t3h/android/elife/api/MainApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import t3h.android.elife.models.Audio;
 import t3h.android.elife.models.Topic;
 
@@ -11,6 +12,6 @@ public interface MainApi {
     @GET("/topic/get-active-topics")
     Call<List<Topic>> getActiveTopicsList();
 
-    @GET("/audio/get-list-by-topic")
-    Call<List<Audio>> getActiveAudiosListByTopic();
+    @GET("/audio/get-list-by-topic/{topicId}")
+    Call<List<Audio>> getActiveAudiosListByTopic(@Path("topicId") int topicId);
 }
