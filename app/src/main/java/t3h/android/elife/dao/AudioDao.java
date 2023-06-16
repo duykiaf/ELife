a300191b2f38e263fa707bbcfeea17b364aba14b
dkifashion <dkiwomanfashion@gmail.com>
2023-06-16 21:39:27 +0700

    build audio database and audio dao

--- app/src/main/java/t3h/android/elife/dao/AudioDao.java
+++ app/src/main/java/t3h/android/elife/dao/AudioDao.java
@@ -1,2 +1,27 @@
-package t3h.android.elife.dao;public interface AudioDao {
+package t3h.android.elife.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+import t3h.android.elife.models.Audio;
+
+@Dao
+public interface AudioDao {
+    @Query("select * from audio")
+    LiveData<List<Audio>> getAllList();
+
+    @Query("select audio.id from audio")
+    LiveData<List<Integer>> getAudioIds();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insert(Audio audio);
+
+    @Delete
+    int delete(Audio audio);
 }
--- app/src/main/java/t3h/android/elife/database/AudioDatabase.java
+++ app/src/main/java/t3h/android/elife/database/AudioDatabase.java
@@ -1,2 +1,24 @@
-package t3h.android.elife.database;public class AudioDatabase {
+package t3h.android.elife.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import t3h.android.elife.dao.AudioDao;
+import t3h.android.elife.models.Audio;
+
+@Database(entities = {Audio.class}, version = 1, exportSchema = false)
+public abstract class AudioDatabase extends RoomDatabase {
+    public abstract AudioDao audioDao();
+
+    private static AudioDatabase INSTANCE;
+
+    public static synchronized AudioDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context, AudioDatabase.class, "audio_db.db").build();
+        }
+        return INSTANCE;
+    }
 }
