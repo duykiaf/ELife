abe2a170f96013b4271274eb3e1e831d0266cc9d
dkifashion <dkiwomanfashion@gmail.com>
2023-06-16 21:46:09 +0700

    define entity object

--- app/src/main/java/t3h/android/elife/models/Audio.java
+++ app/src/main/java/t3h/android/elife/models/Audio.java
@@ -1,35 +1,47 @@
 package t3h.android.elife.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 
+@Entity(tableName = "audio")
 public class Audio implements Serializable {
+    @PrimaryKey
     @SerializedName("id")
     @Expose
     private int id;
 
+    @ColumnInfo(name = "title")
     @SerializedName("title")
     @Expose
     private String title;
 
+    @ColumnInfo(name = "audio_file")
     @SerializedName("audio_file")
     @Expose
     private String audioFile;
 
+    @ColumnInfo(name = "file_name")
     @SerializedName("file_name")
     @Expose
     private String fileName;
 
+    @ColumnInfo(name = "lyrics")
     @SerializedName("lyrics")
     @Expose
     private String lyrics;
 
+    @ColumnInfo(name = "topic_id")
     @SerializedName("topic_id")
     @Expose
     private int topicId;
 
+    @ColumnInfo(name = "status")
     @SerializedName("status")
     @Expose
     private int status;
