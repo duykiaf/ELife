e6fedb77de2ac1552ef01b6fb3940b5b28afc4da
dkifashion <dkiwomanfashion@gmail.com>
2023-06-16 21:42:02 +0700

    build app constants

--- app/src/main/java/t3h/android/elife/helper/AppConstant.java
+++ app/src/main/java/t3h/android/elife/helper/AppConstant.java
@@ -1,2 +1,15 @@
-package t3h.android.elife.helper;public class AppConstant {
+package t3h.android.elife.helper;
+
+public class AppConstant {
+    public static final String DURATION_DEFAULT = "0:00";
+    public static final String BOOKMARKS_LIST = "Bookmarks list";
+    public static final String TOPICS = "Topics";
+    public static final String PLAY_ICON = "PlayIcon";
+    public static final String PAUSE_ICON = "PauseIcon";
+    public static final String BOOKMARK_BORDER_ICON = "BookmarkBorderIcon";
+    public static final String BOOKMARK_ICON = "BookmarkIcon";
+    public static final String ADD_BOOKMARK_SUCCESSFULLY = "Add bookmark successfully!";
+    public static final String ADD_BOOKMARK_FAILED = "Add bookmark failed!";
+    public static final String REMOVE_BOOKMARK_SUCCESSFULLY = "Remove bookmark successfully!";
+    public static final String REMOVE_BOOKMARK_FAILED = "Remove bookmark failed!";
 }
