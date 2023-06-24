42ce13053f6649ca8b78fa3bde67f8ef494f9cfa
dkifashion <dkiwomanfashion@gmail.com>
2023-06-24 10:22:52 +0700

    update base url

--- app/src/main/java/t3h/android/elife/api/ApiProvider.java
+++ app/src/main/java/t3h/android/elife/api/ApiProvider.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiProvider {
-    private static final String BASE_URL = "http://192.168.1.6:3000";
+    private static final String BASE_URL = "http://192.168.1.4:3000";
     private static MainApi mainApi;
     private static Retrofit retrofit;
 
