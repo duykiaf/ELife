5af6c7e317ab61918065f91dfe9209f173746c4d
dkifashion <dkiwomanfashion@gmail.com>
2023-06-24 10:24:28 +0700

    create notification channel

--- app/src/main/java/t3h/android/elife/helper/MainApplication.java
+++ app/src/main/java/t3h/android/elife/helper/MainApplication.java
@@ -1,2 +1,26 @@
-package t3h.android.elife.helper;public class MainApplication {
+package t3h.android.elife.helper;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    AppConstant.CHANNEL_ID, AppConstant.CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(notificationChannel);
+            }
+        }
+    }
 }
