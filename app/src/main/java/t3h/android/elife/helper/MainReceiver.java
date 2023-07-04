ad38e4627c9a1e6ca327c4d8f93aabfe9c3b2635
dkifashion <dkiwomanfashion@gmail.com>
2023-07-04 09:32:52 +0700

    build BroadcaseReceiver to get data from service and pass data to service again

--- app/src/main/java/t3h/android/elife/helper/MainReceiver.java
+++ app/src/main/java/t3h/android/elife/helper/MainReceiver.java
@@ -1,2 +1,31 @@
-package t3h.android.elife.helper;public class MainReceiver {
+package t3h.android.elife.helper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import t3h.android.elife.models.Audio;
+import t3h.android.elife.services.MainService;
+
+public class MainReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent intentToService = new Intent(context, MainService.class);
+
+        Bundle getDataFromIntent = intent.getExtras();
+        int audioAction = getDataFromIntent.getInt("AudioAction");
+        Audio audioInfo = (Audio) getDataFromIntent.get("AudioInfo");
+
+        intentToService.putExtra("AudioActionToService", audioAction);
+        intentToService.putExtra("AudioInfoToService", audioInfo);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intentToService);
+        } else {
+            context.startService(intentToService);
+        }
+    }
 }
