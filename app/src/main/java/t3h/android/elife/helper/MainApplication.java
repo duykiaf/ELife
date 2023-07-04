338f89f632a7cd6f254e2476377d44da4a1c2269
dkifashion <dkiwomanfashion@gmail.com>
2023-07-04 09:30:04 +0700

    update importance param and disabled notification sound

--- app/src/main/java/t3h/android/elife/helper/MainApplication.java
+++ app/src/main/java/t3h/android/elife/helper/MainApplication.java
@@ -15,8 +15,9 @@ public class MainApplication extends Application {
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel(
-                    AppConstant.CHANNEL_ID, AppConstant.CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+                    AppConstant.CHANNEL_ID, AppConstant.CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH
             );
+            notificationChannel.setSound(null, null);
             NotificationManager manager = getSystemService(NotificationManager.class);
             if (manager != null) {
                 manager.createNotificationChannel(notificationChannel);
