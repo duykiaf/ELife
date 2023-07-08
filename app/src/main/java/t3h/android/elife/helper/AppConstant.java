4be8ae97e84fd8304af7c7dcd0a73e56b179abf2
dkifashion <dkiwomanfashion@gmail.com>
2023-07-08 21:46:30 +0700

    delete constants not use

--- app/src/main/java/t3h/android/elife/helper/AppConstant.java
+++ app/src/main/java/t3h/android/elife/helper/AppConstant.java
@@ -1,14 +1,11 @@
 package t3h.android.elife.helper;
 
-import android.graphics.Color;
-
 public class AppConstant {
     public static final String DURATION_DEFAULT = "0:00";
     public static final String BOOKMARKS_LIST = "Bookmarks list";
     public static final String TOPICS = "Topics";
     public static final String PLAY_ICON = "PlayIcon";
     public static final String PAUSE_ICON = "PauseIcon";
-    public static final String UNABLE_ICON = "UnableIcon";
     public static final String BOOKMARK_BORDER_ICON = "BookmarkBorderIcon";
     public static final String BOOKMARK_ICON = "BookmarkIcon";
     public static final String ADD_BOOKMARK_SUCCESSFULLY = "Add bookmark successfully!";
@@ -27,6 +24,4 @@ public class AppConstant {
     public static final String CHANNEL_ID = "t3h.android.elife.ELIFE_CHANNEL";
     public static final String CHANNEL_NAME = "Elife";
     public static final int NOTIFICATION_ID = 1;
-    public static final int DISABLE_COLOR = Color.parseColor("#A298FA");
-    public static final int ENABLE_COLOR = Color.parseColor("#1C89DA");
 }
